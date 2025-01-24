f43d6f0350a68498a044af13dff4b36452fe2243
Pablo de Maya <pablo.cescit@gmail.com>
2025-01-24 16:59:34 +0100

    Primera fase de la tabla

--- src/components/FacturasDeVenta.vue
+++ src/components/FacturasDeVenta.vue
@@ -114,6 +114,36 @@
             </div>
           </div>
         </div>
+        <div
+          class="q-table__middle scroll row justify-between items-center"
+        >
+          <q-table
+            :rows="rows_facturasVenta"
+            :columns="columns_facturasVenta"
+            row-key="name"
+          >
+
+          <template v-slot:body-cell-base_imponible="props">
+            <q-td :props="props">
+              {{ props.value.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' }) }}
+            </q-td>
+          </template>
+
+          <template v-slot:body-cell-total="props">
+            <q-td :props="props">
+              {{ props.value.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' }) }}
+            </q-td>
+          </template>
+
+          <template v-slot:body-cell-fecha_pedido="props">
+            <q-td :props="props">
+              {{ new Date(props.value).toLocaleString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' }) }}
+            </q-td>
+          </template>
+
+          </q-table>
+
+        </div>
       </div>
     </q-card-section>
   </q-card>
@@ -136,6 +166,39 @@ export default {
       'Cobrada'
     ]);
 
+    const columns_facturasVenta = [
+
+  { name: 'numero_factura', align: 'left', label: 'Número de factura', field: 'numero_factura', sortable: true },
+  { name: 'cliente', label: 'Cliente', field: 'cliente', sortable: true },
+  { name: 'referencia', label: 'Referencia', field: 'referencia', sortable: true},
+  { name: 'fecha_factura', label: 'Fecha factura', field: 'fecha_factura', sortable: true },
+  { name: 'base_imponible', label: 'Base imponible', field: 'base_imponible', sortable: true},
+  { name: 'total', label: 'Total', field: 'total', sortable: true},
+  { name: 'estado', label: 'Estado', field: 'estado', sortable: true}
+]
+
+const rows_facturasVenta = [
+  {
+    numero_factura: '2024/00015',
+    cliente: 'Pablo',
+    referencia: '',
+    fecha_factura: '24-01-2025',
+    base_imponible: 220,
+    total: 226.2,
+    estado: '1 Borrador'
+  }
+]
+
+const formatValue = (value) => {
+    if (typeof value === 'number') {
+      return value.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' });
+    } else if (!value) {
+      return 'N/A';
+    }
+    return value;
+  };
+
+
     return {
       fechaInicio,
       fechaFinal,
@@ -143,6 +206,9 @@ export default {
       options_estado,
       search: ref(''),
       right: ref(false),
+      columns_facturasVenta,
+      rows_facturasVenta,
+      formatValue
     }
   }
 }
