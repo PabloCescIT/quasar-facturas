99790f685f851653ed60a4c10a2e7e15854d7b53
Pablo de Maya <pablo.cescit@gmail.com>
2025-01-29 16:21:02 +0100

    Se añade la función "Alta nuevo cliente" al hacer click al botón "add" de "Datos del cliente" en un pop-up.

---
+++ src/components/AltaNuevoCliente.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-6">
+      <q-input
+      class="q-mr-lg"
+      v-model="texto_codigo_cliente"
+      label="Código de cliente"
+      />
+    </div>
+    <div class="col-6">
+      <q-input
+      v-model="texto_nif"
+      label="NIF"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-12">
+      <q-input
+      v-model="texto_nombre"
+      label="Nombre"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-12">
+      <q-input
+      v-model="texto_direccion"
+      label="Dirección"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-6">
+      <q-input
+      class="q-mr-lg"
+      v-model="texto_codigo_postal"
+      label="Código postal"
+      />
+    </div>
+    <div class="col-6">
+      <q-select
+      v-model="select_provincias"
+      :options="options_provincias"
+      label="Provincia"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-6">
+      <q-select
+      class="q-mr-lg"
+      v-model="select_poblacion"
+      :options="options_poblacion"
+      label="Población"
+      />
+    </div>
+    <div class="col-6">
+      <q-input
+      v-model="texto_pais"
+      label="País"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-6">
+      <q-input
+      class="q-mr-lg"
+      v-model="texto_telefono_fijo"
+      label="Teléfono fijo"
+      />
+    </div>
+    <div class="col-6">
+      <q-input
+      v-model="texto_telefono_movil"
+      label="Teléfono móvil"
+      />
+    </div>
+  </div>
+
+  <div class="row justify-between items-center q-mb-md">
+    <div class="col-6">
+      <q-input
+      class="q-mr-lg"
+      v-model="texto_email"
+      label="E-mail"
+      />
+    </div>
+    <div class="col-6">
+      <q-input
+      v-model="texto_web"
+      label="Web"
+      />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-12 text-right">
+      <q-btn
+        class="q-mt-xl"
+        color="primary"
+        label="Guardar"
+      />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { ref } from 'vue';
+
+
+export default {
+
+  components: {
+
+  },
+
+  setup() {
+
+
+    return {
+
+      select_provincias: ref(null),
+      options_provincias: [
+        'Álava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Ávila', 'Badajoz', 'Baleares', 'Barcelona', 'Bizkaia', 'Burgos', 'Cáceres', 'Cádiz', 'Cantabria', 'Castellón', 'Ceuta', 'Ciudad Real', 'Córdoba', 'Cuenca', 'Girona', 'Granada', 'Guadalajara', 'Gipuzkoa', 'Huelva', 'Huesca', 'Jaén', 'La Coruña', 'La Rioja', 'Las Palmas', 'León', 'Lleida', 'Lugo', 'Madrid', 'Málaga', 'Melilla', 'Murcia', 'Navarra', 'Ourense', 'Palencia', 'Pontevedra', 'Salamanca', 'Santa Cruz de Tenerife', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Zamora', 'Zaragoza'
+      ],
+
+      select_poblacion: ref(null),
+      options_poblacion: [
+        'Albacete', 'Alicante', 'Almería', 'Ávila', 'Badajoz', 'Barcelona', 'Bilbao', 'Burgos', 'Cáceres', 'Cádiz', 'Castellón de la Plana', 'Ceuta', 'Ciudad Real', 'Córdoba', 'Cuenca', 'Gerona', 'Granada', 'Guadalajara', 'Huelva', 'Huesca', 'Jaén', 'Las Palmas de Gran Canaria', 'León', 'Lérida', 'Logroño', 'Lugo', 'Madrid', 'Málaga', 'Melilla', 'Murcia', 'Orense', 'Oviedo', 'Palencia', 'Palma de Mallorca', 'Pamplona', 'Pontevedra', 'Salamanca', 'San Sebastián', 'Santa Cruz de Tenerife', 'Santander', 'Segovia', 'Sevilla', 'Soria', 'Tarragona', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vitoria', 'Zamora', 'Zaragoza'
+      ]
+
+    };
+  },
+};
+</script>
+
--- src/components/DatosCliente.vue
+++ src/components/DatosCliente.vue
@@ -1,92 +1,109 @@
 <template>
   <div>
-    <div
-      class="text-weight-bold"
-    >
+    <div class="text-weight-bold">
       Datos del cliente
     </div>
     <div style="display: flex; align-items: center; justify-content: space-between;">
+      <q-select
+        filled
+        v-model="buscar_cliente"
+        use-input
+        hide-selected
+        fill-input
+        label="Buscar cliente"
+        input-debounce="0"
+        :options="options_cliente"
+        @filter="filterFn"
+        style="width: 450px;"
+        hint="Escribe para buscar"
+      >
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              No existe este cliente
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
 
-        <q-select
-          filled
-          v-model="buscar_cliente"
-          use-input
-          hide-selected
-          fill-input
-          label="Buscar cliente"
-          input-debounce="0"
-          :options="options_cliente"
-          @filter="filterFn"
-          style="width: 450px;"
-          hint="Escribe para buscar"
-        >
-
-          <template v-slot:no-option>
-            <q-item>
-              <q-item-section class="text-grey">
-                No existe este cliente
-              </q-item-section>
-            </q-item>
-          </template>
-
-        </q-select>
-
-          <div
-            class="items-center q-mb-md"
-          >
-            <q-btn
-              round
-              color="primary"
-              icon="add"
-            />
-          </div>
-
-          <div
-            class="items-center q-mb-md"
-          >
-            <q-btn
-              flat
-              round
-              color="primary"
-              icon="close"
-            />
-          </div>
+      <div class="items-center q-mb-md">
+        <q-btn
+          round
+          color="primary"
+          icon="add"
+          @click="showAltaNuevoCliente = true"
+        />
       </div>
+
+      <div class="items-center q-mb-md">
+        <q-btn
+          flat
+          round
+          color="primary"
+          icon="close"
+        />
+      </div>
+
+      <q-dialog v-model="showAltaNuevoCliente">
+        <q-card style="width:30vw; max-width: 100%;">
+          <q-card-section class="q-py-sm q-px-sm bg-primary" style="display: flex; align-items: center; justify-content: space-between;">
+            <div class="text-white text-h6 q-pa-sm">
+              Alta nuevo cliente
+            </div>
+            <div style="display: flex; align-items: center; gap: 10px;">
+              <q-btn
+                flat
+                round
+                icon="close"
+                color="white"
+                v-close-popup
+              />
+            </div>
+          </q-card-section>
+          <q-card-section
+            class="q-ma-md"
+          >
+            <AltaNuevoCliente/>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
-
-const clienteOptions = [
-  'Pablo', 'Carles', 'Alejandro', 'Oscar'
-];
+import AltaNuevoCliente from './AltaNuevoCliente.vue';
 
 export default {
+
+  components: {
+    AltaNuevoCliente
+  },
+
   setup() {
+    const clienteOptions = ['Pablo', 'Carles', 'Alejandro', 'Oscar'];
     const options_cliente = ref(clienteOptions);
+    const buscar_cliente = ref(null);
+    const showAltaNuevoCliente = ref(false);
 
     const filterFn = (val, update, abort) => {
       if (val.length < 2) {
         abort();
         return;
       }
-
       update(() => {
         const needle = val.toLowerCase();
-        options_cliente.value = clienteOptions.filter(v => v.toLowerCase().indexOf(needle) > -1);
+        options_cliente.value = clienteOptions.filter(v => v.toLowerCase().includes(needle));
       });
     };
 
     return {
-      buscar_cliente: ref(null),
+      buscar_cliente,
       options_cliente,
       filterFn,
+      showAltaNuevoCliente,
     };
   },
 };
 </script>
-
-<style scoped>
-
-</style>
